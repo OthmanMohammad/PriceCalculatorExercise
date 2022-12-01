89019bc5cb1d6c71b41050ba2e9a3df890d75670
Mohammad Othman <emranm226@hotmail.com>
2022-12-01 07:11:20 +0000

    creating class for product and implementing tax

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PriceCalculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Amount amount = new Amount(5);
+
+        }
+
+    }
+}
---
+++ Tax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculator
+{
+    public class Tax
+    {
+        public readonly int Percent;
+        public double TaxRate { get;}
+        public Tax(int percent)
+        {
+            if (percent < 0 || percent > 100)
+                throw new ArgumentOutOfRangeException(
+                    nameof(percent),
+                    $"{nameof(Tax)} percentage should be greater than 0 and less than 100");
+
+            this.Percent = percent;
+            TaxRate = (double)Percent / 100;
+        }
+        public override string ToString() => $"{Percent}%";
+    }
+}
